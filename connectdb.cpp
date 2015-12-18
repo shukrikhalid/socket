7efa8d728461ade989f5b48a81d5642f7d6c836b
shukrikhalid <shukri.khalidgm@gmail.com>
2015-12-18 10:49:16 -0500

    connect database postgresql

--- connectdb.cpp
+++ connectdb.cpp
@@ -7,8 +7,8 @@ using namespace pqxx;
 int main(int argc, char* argv[])
 {
    try{
-      connection C("dbname=testdb user=postgres password=cohondob \
-      hostaddr=127.0.0.1 port=5432");
+      connection C("dbname=uitm user=postgres password=root \
+      hostaddr=192.168.56.101 port=5432");
       if (C.is_open()) {
          cout << "Opened database successfully: " << C.dbname() << endl;
       } else {
